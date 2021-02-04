ab6f2ce3a7ce5f8c659592da9a3d35b67404d8c6
Doode1524 <Doode1524@gmail.com>
2021-02-04 17:17:58 -0500

    add name column to users and add strong params

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => {:registrations => "registrations"}
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root to: 'pages#index'
@@ -7,5 +7,5 @@ Rails.application.routes.draw do
   devise_scope :user do
     get 'login', to: 'devise/sessions#new'
   end
-  
+
 end
