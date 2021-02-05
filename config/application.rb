9c44f4331262a987d2d8d58979ce9a004bd34146
Doode1524 <Doode1524@gmail.com>
2021-02-04 19:30:57 -0500

    signed in or out collapsable links/helpers-partials

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,8 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+config.action_controller.include_all_helpers = false
+
 module RailsJsNewProject
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
